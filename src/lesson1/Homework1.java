c08585dbf01bc7a8b0a7349acb54f0fdc7eac409
Olga <o.a.chudaykina@yandex.ru>
2024-03-28 22:59:18 +0300

    First commit

---
+++ src/lesson1/Homework1.java
@@ -0,0 +1,20 @@
+package lesson1;
+
+public class Homework1 {
+    public static void main(String[] args) {
+
+        int money = 1000;
+        int priceOfSnickers = 35;
+        double priceOfCandy = 3.5;
+
+        int countSnickersCanBuy = money / priceOfSnickers;
+        int restOfMoneyAfterSnickersBuy = money - (countSnickersCanBuy * priceOfSnickers);
+
+        int countCandyBuy = (int)(restOfMoneyAfterSnickersBuy / priceOfCandy);
+        double restOfMoney = restOfMoneyAfterSnickersBuy - (countCandyBuy * priceOfCandy);
+
+        System.out.println(countSnickersCanBuy);
+        System.out.println(countCandyBuy);
+        System.out.println(restOfMoney);
+    }
+}
