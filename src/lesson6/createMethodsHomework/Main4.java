ea7bb4c183e5a43e69c27b3667beb5e9d1ffeab6
Olga <o.a.chudaykina@yandex.ru>
2024-03-31 23:21:12 +0300

    Added lesson6. В метод передаем 3 числа, метод должен вернуть меньшее число из 3-х

---
+++ src/lesson6/createMethodsHomework/Main4.java
@@ -0,0 +1,8 @@
+package lesson6.createMethodsHomework;
+
+public class Main4 {
+    public static void main(String[] args) {
+        int numbers = AllMethodsInHomework.findTheMinimumNumber();
+        System.out.println("Меньшее число из трех: " + numbers);
+    }
+}
