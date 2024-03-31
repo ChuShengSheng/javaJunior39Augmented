861b8776041573635907a67b174f68686ac8df7e
Olga <o.a.chudaykina@yandex.ru>
2024-03-31 23:36:17 +0300

    Added lesson6. В метод передаем email, метод должен вернуть true или false подходит нам email или нет. Подходит: когда содержит @, когда нет пробелов, когда часть текста после @ содержит в себе точку

---
+++ src/lesson6/createMethodsHomework/Main6.java
@@ -0,0 +1,8 @@
+package lesson6.createMethodsHomework;
+
+public class Main6 {
+    public static void main(String[] args) {
+        String email = AllMethodsInHomework.checkTheValidityOfTheEmail("masyanya@mail.ru");
+        System.out.println("Email подходит!!!");
+    }
+}
