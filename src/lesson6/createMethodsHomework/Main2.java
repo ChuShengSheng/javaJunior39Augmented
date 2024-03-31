dccbb11845fc2d9a5824e32940324bfeba244930
Olga <o.a.chudaykina@yandex.ru>
2024-03-31 22:53:35 +0300

    Added lesson6. Метод принимает входящими параметрами текст, и печатает на консоль сколько в тексте знаков пунктуации

---
+++ src/lesson6/createMethodsHomework/Main2.java
@@ -0,0 +1,8 @@
+package lesson6.createMethodsHomework;
+
+public class Main2 {
+    public static void main(String[] args) {
+        AllMethodsInHomework.PrintPunctuationCharacterInString
+                ("Ingredients: Aqua, Glycerin, Camellia Sinensis Leaf Extract, Parfum.");
+    }
+}
