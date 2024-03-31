f36ff73960caa497f64a5db49e968b7aead438de
Olga <o.a.chudaykina@yandex.ru>
2024-03-31 22:40:22 +0300

    Added lesson6.
    Метод принимает входящими параметрами текст,
    и возвращает кол-во гласных букв в тексте

---
+++ src/lesson6/createMethodsHomework/Main.java
@@ -0,0 +1,9 @@
+package lesson6.createMethodsHomework;
+
+public class Main {
+    public static void main(String[] args) {
+        int countOfVowels = AllMethodsInHomework.totalCountOfVowels("Java programming language");
+        // отобразить общее количество гласных в строке
+        System.out.println("Общее количество гласных в строке равно: " + countOfVowels);
+    }
+}
