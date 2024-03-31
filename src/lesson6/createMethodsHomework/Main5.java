ab7bd69b054e890376da389f2f5e6c9ad7ed5bf3
Olga <o.a.chudaykina@yandex.ru>
2024-03-31 23:32:10 +0300

    Added lesson6. * В метод передаем сумму депозита, банковский процент и кол-во лет, и метод возвращает значение прироста процентов которые мы заберем с банка через столько лет

---
+++ src/lesson6/createMethodsHomework/Main5.java
@@ -0,0 +1,8 @@
+package lesson6.createMethodsHomework;
+
+public class Main5 {
+    public static void main(String[] args) {
+        double allMoney = AllMethodsInHomework.interestGrowth();
+        System.out.println("Значение прироста процентов: " + allMoney);
+    }
+}
