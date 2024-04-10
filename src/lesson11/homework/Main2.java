ae057939fb9fbdcf5aa8f3fb0edd52a6756629d3
Olga <o.a.chudaykina@yandex.ru>
2024-04-11 00:12:50 +0300

    Added lesson11. Вы работаете над программой-каталогом для библиотеки. 1. Разработать классы для хранения информации о печатных изданиях. -книгах (название, автор, год издания, кол-во страниц, издательство); -журналах (название, год выхода, номер (в году), издательство). 2. Создать массив печатных изданий с неоднородным составом (хранить как книги, так и журналы), заполните его несколькими элементами, вывести все элементы этого массива на экран. 3. Найти в массиве самое толстое издание и вывести информацию о нем на экран. 4. Пояснить, где и зачем в данной программе используется полиморфизм.

---
+++ src/lesson11/homework/Main2.java
@@ -0,0 +1,10 @@
+package lesson11.homework;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Library library = new Library();
+        library.fillEditions();
+        library.printAllEditions();
+        library.printLargestBook();
+    }
+}
