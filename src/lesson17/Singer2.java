90dad871ed4fcf5f7d3bab791eb40566d49400e6
Olga <o.a.chudaykina@yandex.ru>
2024-05-01 17:37:19 +0300

    Added lesson17. Многопоточность. Ведущий, певцы.

---
+++ src/lesson17/Singer2.java
@@ -0,0 +1,32 @@
+package lesson17;
+
+public class Singer2 extends Thread{
+
+
+    @Override
+    public void run() {
+        while (true) {
+
+            try {
+                synchronized (Monitors.MICROFON){
+                    Monitors.MICROFON.wait();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            for (int i = 0; i < 3; i++) {
+                System.out.println(" ----------- FA");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            synchronized (Monitors.MICROFON){
+                Monitors.MICROFON.notify();
+            }
+        }
+    }
+}
